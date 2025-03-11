dad451240b005672597129214d4f57d6b462fa1e
Dart Basic to Advanced <mhlokman10@gmail.com>
2025-03-11 14:41:45 +0600

    Final Verdict

--- A_Final_Verdict.cpp
+++ A_Final_Verdict.cpp
@@ -3,11 +3,29 @@
 using namespace std;
 
 int main(){
-    int n, a[n];
+    int t;
+    cin >> t;
 
+    while(t--){
+        int n, x , a;
+        cin >> n >> x;
 
+        int sum =0;
 
+        for(int i =0 ; i<n ; i++ )
+        {
+            cin >> a;
+            sum += a;
+        }
 
+        if(sum/n == x && sum % n ==0)
+        {
+          cout << "YES" << endl;
+        }
+        else
+        cout << " NO" << endl;
+
+    }
 
     return 0;
 }
